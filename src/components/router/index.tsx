29e62273ec6461e3563aae0823f401e4551e6c57
tjduddn8511 <130065523+tjduddn8511@users.noreply.github.com>
2024-08-20 20:47:33 +0900

    feat

--- src/components/router/index.tsx
+++ src/components/router/index.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GlobalStyles } from "../../styles/global.ts";
 import { StyleSheetManager } from "styled-components";
-import CheckStudent from "src/pages/student/index.tsx";
+import CheckStudent from "src/pages/student/";
 
 const Router = () => {
   return (
