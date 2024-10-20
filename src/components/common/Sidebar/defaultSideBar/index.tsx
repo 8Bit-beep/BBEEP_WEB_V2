0d86d699f4b6c5fd945ea310db404f34312ca505
tjduddn8511 <130065523+tjduddn8511@users.noreply.github.com>
2024-10-20 18:15:14 +0900

    fix :: modal view fix#13

--- src/components/common/Sidebar/defaultSideBar/index.tsx
+++ src/components/common/Sidebar/defaultSideBar/index.tsx
@@ -18,7 +18,7 @@ const Sidebar = () => {
     <S.SidebarWrap>
       <S.MenuWrap>
         <S.MenuTitle>
-          <img src={SidebarStudent} />
+          <img src={SidebarStudent} alt="student" />
           <span>학생 조회하기</span>
         </S.MenuTitle>
         <S.MenuItem>
@@ -26,57 +26,57 @@ const Sidebar = () => {
             onClick={() => handleCategoryClick("1학년")}
             $isclicked={isClickCategory === "1학년" ? "true" : "false"}
           >
-            <img src={One} />
+            <img src={One} alt="1grade"/>
             <span>1학년</span>
           </S.Item>
           <S.Item
             onClick={() => handleCategoryClick("2학년")}
             $isclicked={isClickCategory === "2학년" ? "true" : "false"}
           >
-            <img src={Two} />
+            <img src={Two} alt="2grade" />
             <span>2학년</span>
           </S.Item>
           <S.Item
             onClick={() => handleCategoryClick("3학년")}
             $isclicked={isClickCategory === "3학년" ? "true" : "false"}
           >
-            <img src={Three} />
+            <img src={Three}  alt="3grade"/>
             <span>3학년</span>
           </S.Item>
         </S.MenuItem>
         <S.MenuTitle>
-          <img src={SidebarLab} />
+          <img src={SidebarLab} alt="room" />
           <span>실 조회햐기</span>
         </S.MenuTitle>
         <S.MenuItem style={{ minHeight: "75%" }}>
           <S.Item onClick={() => handleCategoryClick("1층")} $isclicked={isClickCategory === "1층" ? "true" : "false"}>
-            <img src={One} />
+            <img src={One} alt="1floor"  />
             <span>1층</span>
           </S.Item>
           <S.Item onClick={() => handleCategoryClick("2층")} $isclicked={isClickCategory === "2층" ? "true" : "false"}>
-            <img src={Two} />
+            <img src={Two} alt="2floor" />
             <span>2층</span>
           </S.Item>
           <S.Item onClick={() => handleCategoryClick("3층")} $isclicked={isClickCategory === "3층" ? "true" : "false"}>
-            <img src={Three} />
+            <img src={Three} alt="3floor" />
             <span>3층</span>
           </S.Item>
         </S.MenuItem>
         <S.MenuTitle>
-          <img src={Check} />
+          <img src={Check} alt="check" />
           <span>출석 체크하기</span>
         </S.MenuTitle>
         <S.MenuItem style={{ minHeight: "75%" }}>
           <S.Item onClick={() => handleCategoryClick("2출석")} $isclicked={isClickCategory === "2출석" ? "true" : "false"}>
-            <img src={Two} />
+            <img src={Two} alt="2floor" />
             <span>2층</span>
           </S.Item>
           <S.Item onClick={() => handleCategoryClick("3출석")} $isclicked={isClickCategory === "3출석" ? "true" : "false"}>
-            <img src={Three} />
+            <img src={Three}  alt="3floor"/>
             <span>3층</span>
           </S.Item>
           <S.Item onClick={() => handleCategoryClick("전출석")} $isclicked={isClickCategory === "전출석" ? "true" : "false"}>
-            <img src={Check} />
+            <img src={Check}  alt="whole"/>
             <span>전체</span>
           </S.Item>
         </S.MenuItem>
--- src/modal/index.tsx
+++ src/modal/index.tsx
@@ -21,52 +21,43 @@ const Modal = ({ onClick, cls = [], title, manage }: Props) => {
             <img style={{ width: 40, height: 40 }} src={Back} onClick={() => onClick("back")} />
             <S.ModalTitleWrapper>{title}</S.ModalTitleWrapper>
           </S.HeaderLineWrapper>
-          <S.ModalListWrapper
-            style={Array.isArray(manage) && manage.length > 0 ? { gap: "0", borderRadius: 50, width: "120%" } : {}}
-          >
+          <S.ModalListWrapper>
             {Array.isArray(manage) && manage.length > 0 ? (
               <>
-                {["", ""].map((it, i) => {
-                  return (
-                    <S.ManageListItem style={{ backgroundColor: "#F1F1F1FF", height: "11%", borderRadius: 10 }}>
-                      <span style={{ width: "17%", height: "40%", textAlign: "center" }}>학번</span>
-                      <span style={{ width: "17%", height: "40%", textAlign: "center" }}>이름</span>
-                      <span style={{ width: "41%", height: "40%", textAlign: "center" }}>시간</span>
-                      <span style={{ width: "21%", height: "40%", textAlign: "end" }}>출결정보</span>
-                    </S.ManageListItem>
-                  );
-                })}
-                {manage.map((item, idx) => (
-                  <S.ManageListItem
-                    key={idx}
-                    style={{
-                      borderStyle: "solid",
-                      borderWidth: 0,
-                      borderRightWidth: idx % 2 ? 0 : 5,
-                      borderColor: "#f1f1f1",
-                    }}
-                  >
-                    <span style={{ width: "17%", height: "40%", textAlign: "center" }}>
-                      {item.grade}
-                      {item.cls}
-                      {item.num?.toString().padStart(2, "0")}
-                    </span>
-                    <span style={{ width: "17%", height: "40%", textAlign: "center" }}>{item.name}</span>
-                    <span style={{ width: "17%", height: "40%", textAlign: "center" }}>
-                      {item.modifiedDate?.toString().split("T")[1]}
-                      {/* {dayjs(item.modifiedDate)} */}
-                    </span>
-                    {item.name && (
-                      <span style={{ width: "42.5%", height: "40%", textAlign: "end" }}>
-                        {item.isExist ? (
-                          <span style={{ color: "#24b105", fontWeight: "600", fontSize: 15 }}>출석</span>
-                        ) : (
-                          <span style={{ color: "red", fontWeight: "600", fontSize: 15 }}>결석</span>
-                        )}
-                      </span>
-                    )}
-                  </S.ManageListItem>
-                ))}
+                <S.ManageListTitleWrapper>
+                  <S.ManageListTitleContentWrapper>
+                    <S.TitleSpan>학번</S.TitleSpan>
+                    <S.TitleSpan>이름</S.TitleSpan>
+                    <S.TitleSpan>마지막 출석</S.TitleSpan>
+                    <S.TitleSpan>8교시 출석</S.TitleSpan>
+                    <S.TitleSpan>9교시 출석</S.TitleSpan>
+                    <S.TitleSpan>10교시 출석</S.TitleSpan>
+                    <S.TitleSpan>11교시 출석</S.TitleSpan>
+                    <S.TitleSpan>출/결</S.TitleSpan>
+                  </S.ManageListTitleContentWrapper>
+                </S.ManageListTitleWrapper>
+                <S.ManageListItem>
+                  {manage.map((item, idx) => (
+                    <S.ManageListContentItemWrapper>
+                      <S.TitleSpan key={idx}>
+                        {item.grade}
+                        {item.cls}
+                        {item.num?.toString().padStart(2, "0")}
+                      </S.TitleSpan>
+                      <S.TitleSpan>{item.name}</S.TitleSpan>
+                      <S.TitleSpan>{item.modifiedDate?.toString().split("T")[0]}</S.TitleSpan>
+                      <S.TitleSpan>{item.modifiedDate?.toString().split("T")[0]}</S.TitleSpan>
+                      <S.TitleSpan>{item.modifiedDate?.toString().split("T")[0]}</S.TitleSpan>
+                      <S.TitleSpan>{item.modifiedDate?.toString().split("T")[0]}</S.TitleSpan>
+                      <S.TitleSpan>{item.modifiedDate?.toString().split("T")[0]}</S.TitleSpan>
+                      {item.isExist === true ? (
+                        <S.TitleSpan style={{ color: "#24b105" }}>출석</S.TitleSpan>
+                      ) : (
+                        <S.TitleSpan style={{ color: "red" }}>결석</S.TitleSpan>
+                      )}
+                    </S.ManageListContentItemWrapper>
+                  ))}
+                </S.ManageListItem>
               </>
             ) : (
               cls?.map((item, idx) => (
