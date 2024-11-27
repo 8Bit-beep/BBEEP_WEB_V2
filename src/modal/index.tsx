a742abaa17eb259ec8ec44b8590b41ddf9a5db3d
전민찬 <132974029+wjzlskxk@users.noreply.github.com>
2024-11-27 19:35:08 +0900

    fix :: isExist

--- src/modal/index.tsx
+++ src/modal/index.tsx
@@ -1,8 +1,8 @@
-import { ClassResponseType } from "src/types/class/class.type";
-import * as S from "./style";
-import Back from "src/assets/Class/Modal/BackButton.svg";
-import { ClassManagement } from "src/types/management/studentManagement.type";
-import dayjs from "dayjs";
+import { ClassResponseType } from 'src/types/class/class.type';
+import * as S from './style';
+import Back from 'src/assets/Class/Modal/BackButton.svg';
+import { ClassManagement } from 'src/types/management/studentManagement.type';
+import dayjs from 'dayjs';
 
 interface Props {
   onClick: (item: string) => void;
@@ -12,11 +12,10 @@ interface Props {
 }
 
 const Modal = ({ onClick, cls = [], title, manage }: Props) => {
+  console.log(manage);
 
-  console.log(manage)
-
-  if(manage) {
-    console.log(manage[0]?.todayLastLogs[0])
+  if (manage) {
+    console.log(manage[0]?.todayLastLogs[0]);
   }
 
   return (
@@ -24,7 +23,7 @@ const Modal = ({ onClick, cls = [], title, manage }: Props) => {
       <S.ModalWrapper>
         <S.ModalMainWrapper>
           <S.HeaderLineWrapper>
-            <img style={{ width: 40, height: 40 }} src={Back} onClick={() => onClick("back")} alt="img" />
+            <img style={{ width: 40, height: 40 }} src={Back} onClick={() => onClick('back')} alt="img" />
             <S.ModalTitleWrapper>{title}</S.ModalTitleWrapper>
           </S.HeaderLineWrapper>
           <S.ModalListWrapper>
@@ -32,162 +31,157 @@ const Modal = ({ onClick, cls = [], title, manage }: Props) => {
               <>
                 <S.ManageListTitleWrapper>
                   <div
-                      style={{
-                        width: "12%",
-                        height: "100%",
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                      }}
+                    style={{
+                      width: '12%',
+                      height: '100%',
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}
                   >
                     <S.TitleSpan>학번</S.TitleSpan>
                   </div>
                   <div
-                      style={{
-                        width: "10%",
-                        height: "100%",
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                      }}
+                    style={{
+                      width: '10%',
+                      height: '100%',
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}
                   >
                     <S.TitleSpan>이름</S.TitleSpan>
                   </div>
                   <div
-                      style={{
-                        width: "12%",
-                        height: "100%",
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                      }}
+                    style={{
+                      width: '12%',
+                      height: '100%',
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}
                   >
                     <S.TitleSpan>마지막 출석</S.TitleSpan>
                   </div>
-                  {
-                    ['8','9','10','11'].map((item) => (
-                        <>
-                          <div
-                              style={{
-                                width: "12.5%",
-                                height: "100%",
-                                display: "flex",
-                                alignItems: "center",
-                                justifyContent: "center",
-                              }}
-                          >
-                            <S.TitleSpan>{`${item}`}교시 출석</S.TitleSpan>
-                          </div>
-                          <div
-                              style={{
-                                width: "11.7%",
-                                height: "100%",
-                                display: "flex",
-                                alignItems: "center",
-                                justifyContent: "center",
-                              }}
-                          >
-                            <S.TitleSpan>출결</S.TitleSpan>
-                          </div>
-                        </>
-                    ))
-                  }
+                  {['8', '9', '10', '11'].map((item) => (
+                    <>
+                      <div
+                        style={{
+                          width: '12.5%',
+                          height: '100%',
+                          display: 'flex',
+                          alignItems: 'center',
+                          justifyContent: 'center',
+                        }}
+                      >
+                        <S.TitleSpan>{`${item}`}교시 출석</S.TitleSpan>
+                      </div>
+                      <div
+                        style={{
+                          width: '11.7%',
+                          height: '100%',
+                          display: 'flex',
+                          alignItems: 'center',
+                          justifyContent: 'center',
+                        }}
+                      >
+                        <S.TitleSpan>출결</S.TitleSpan>
+                      </div>
+                    </>
+                  ))}
                 </S.ManageListTitleWrapper>
                 <div
-                    style={{width: "100%", height: 500, display: "flex", overflowY: "scroll", flexDirection: "column"}}
+                  style={{ width: '100%', height: 500, display: 'flex', overflowY: 'scroll', flexDirection: 'column' }}
                 >
                   {manage.map((item, idx) => (
-                      <S.ManageListItem key={idx}>
-                        <div
-                            style={{
-                              width: "12%",
-                              height: 80,
-                              display: "flex",
-                              alignItems: "center",
-                              justifyContent: "center",
-                            }}
-                        >
-                          <S.TitleSpan>
-                            {item.grade}
-                            {item.cls}
-                            {item.num?.toString().padStart(2, "0")}
-                          </S.TitleSpan>
-                        </div>
-                        <div
-                            style={{
-                              width: "10%",
-                              height: 80,
-                              display: "flex",
-                              alignItems: "center",
-                              justifyContent: "center",
-                            }}
-                        >
-                          <S.TitleSpan>{item.name}</S.TitleSpan>
-                        </div>
-                        <div
-                            style={{
-                              width: "13.5%",
-                              height: 80,
-                              display: "flex",
-                              alignItems: "center",
-                              justifyContent: "center",
-                            }}
-                        >
-                          <S.TitleSpan>
-                            {item?.modifiedDate
-                                ? dayjs(item.modifiedDate).format("hh:mm:ss")
-                                : "출석기록 없음"}
-                          </S.TitleSpan>
-                        </div>
-                        {
-                          ["EIGTH", "NINE", "TEN", "ELEVEN"].map((key, index) => {
-                            return item.todayLastLogs[index] || { lastUpdated: null, position: key };
-                          }).map((item) => (
-                              <>
-                                <div
-                                    style={{
-                                      width: "13.5%",
-                                      height: 80,
-                                      display: "flex",
-                                      alignItems: "center",
-                                      justifyContent: "center",
-                                    }}
-                                >
-                                  <S.TitleSpan>
-                                    {item.lastUpdated
-
-                                        ? dayjs(item.lastUpdated).format("hh:mm:ss")
-                                        : "출석기록 없음"}
-                                  </S.TitleSpan>
-                                </div>
-                                <div
-                                    style={{
-                                      width: "12%",
-                                      height: 80,
-                                      display: "flex",
-                                      alignItems: "center",
-                                      justifyContent: "center",
-                                    }}
-                                >
-                                  {item.lastUpdated ? (
-                                      <S.TitleSpan style={{color: "#24b105"}}>출석</S.TitleSpan>
-                                  ) : (
-                                      <S.TitleSpan style={{color: "red"}}>결석</S.TitleSpan>
-                                  )}
-                                </div>
-                              </>
-                          ))
-                        }
-                      </S.ManageListItem>
+                    <S.ManageListItem key={idx}>
+                      <div
+                        style={{
+                          width: '12%',
+                          height: 80,
+                          display: 'flex',
+                          alignItems: 'center',
+                          justifyContent: 'center',
+                        }}
+                      >
+                        <S.TitleSpan>
+                          {item.grade}
+                          {item.cls}
+                          {item.num?.toString().padStart(2, '0')}
+                        </S.TitleSpan>
+                      </div>
+                      <div
+                        style={{
+                          width: '10%',
+                          height: 80,
+                          display: 'flex',
+                          alignItems: 'center',
+                          justifyContent: 'center',
+                        }}
+                      >
+                        <S.TitleSpan>{item.name}</S.TitleSpan>
+                      </div>
+                      <div
+                        style={{
+                          width: '13.5%',
+                          height: 80,
+                          display: 'flex',
+                          alignItems: 'center',
+                          justifyContent: 'center',
+                        }}
+                      >
+                        <S.TitleSpan>
+                          {item?.modifiedDate ? dayjs(item.modifiedDate).format('hh:mm:ss') : '출석기록 없음'}
+                        </S.TitleSpan>
+                      </div>
+                      {['EIGTH', 'NINE', 'TEN', 'ELEVEN']
+                        .map((key, index) => {
+                          return item || { lastUpdated: null, position: key };
+                        })
+                        .map((item) => (
+                          <>
+                            <div
+                              style={{
+                                width: '13.5%',
+                                height: 80,
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                              }}
+                            >
+                              <S.TitleSpan>
+                                {item.todayLastLogs[idx]?.lastUpdated
+                                  ? dayjs(item.todayLastLogs[idx].lastUpdated).format('hh:mm:ss')
+                                  : '출석기록 없음'}
+                              </S.TitleSpan>
+                            </div>
+                            <div
+                              style={{
+                                width: '12%',
+                                height: 80,
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                              }}
+                            >
+                              {item.isExist ? (
+                                <S.TitleSpan style={{ color: '#24b105' }}>출석</S.TitleSpan>
+                              ) : (
+                                <S.TitleSpan style={{ color: 'red' }}>결석</S.TitleSpan>
+                              )}
+                            </div>
+                          </>
+                        ))}
+                    </S.ManageListItem>
                   ))}
                 </div>
               </>
             ) : (
-                cls?.map((item, idx) => (
-                    <S.ListItem key={idx}>
-                      {item.grade} 학년 {item.cls} 반 {item.name}
-                    </S.ListItem>
-                ))
+              cls?.map((item, idx) => (
+                <S.ListItem key={idx}>
+                  {item.grade} 학년 {item.cls} 반 {item.name}
+                </S.ListItem>
+              ))
             )}
           </S.ModalListWrapper>
         </S.ModalMainWrapper>
